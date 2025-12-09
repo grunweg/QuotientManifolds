0f93403c70bd12f33b65e48d5be337a6529fbf34
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-12-09 12:40:53 +0100

    Sketch the basic project statements

--- QuotientManifolds/Basic.lean
+++ QuotientManifolds/Basic.lean
@@ -1,1 +1,41 @@
-def hello := "world"
+import Mathlib.Geometry.Manifold.Diffeomorph
+import Mathlib.Topology.Algebra.ProperAction.ProperlyDiscontinuous
+
+-- See `DifferentialGeometry.lean` for a quick overview to differential geometry in Lean.
+
+-- `M` be a smooth manifold, modelled over the pair `(E, H)`
+variable {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  {H : Type*} [TopologicalSpace H] (I : ModelWithCorners 𝕜 E H)
+  {M : Type*} [TopologicalSpace M] [ChartedSpace H M] {n : ℕ∞}
+  [IsManifold I n M]
+
+-- Let `G` be a group acting properly discontinuously on `M`.
+variable {G : Type*} [Group G] [MulAction G M] [ProperlyDiscontinuousSMul G M]
+
+-- Consider the quotient space `M / G`. For now, let's give this a special name.
+variable (G M) in
+abbrev OrbitSpace := MulAction.orbitRel.Quotient G M
+
+-- This is the quotient map from `M` to the orbit space `M / G`.
+example : M → OrbitSpace M G := Quotient.mk _
+
+-- Mathlib already knows this is a topological space.
+example : TopologicalSpace (OrbitSpace M G) := by infer_instance
+
+-- Let's define a charted space structure on the quotient.
+
+-- Need to prove some well-definedness, and use the condition on the group action.
+
+instance : ChartedSpace H (OrbitSpace M G) where
+  atlas := sorry
+  chartAt := sorry
+  mem_chart_source := sorry
+  chart_mem_atlas := sorry
+
+-- And let's prove that it's a manifold.
+instance : IsManifold I n (OrbitSpace M G) := sorry
+
+-- Once we have done this, let's prove that the projection map is smooth.
+lemma contMDiff_quotientMk : ContMDiff I I n (Quotient.mk _ : M → OrbitSpace M G) := by
+  sorry
