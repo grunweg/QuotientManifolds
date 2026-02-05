6bfacd07ba79ce23643200e30338456157578323
pepamontero <pepamonterojimena@gmail.com>
2026-02-05 16:26:20 +0000

    resolve conflicts

--- QuotientManifolds/Basic.lean
+++ QuotientManifolds/Basic.lean
@@ -2,6 +2,7 @@ import Mathlib.Geometry.Manifold.Diffeomorph
 import Mathlib.Topology.Algebra.ProperAction.CompactlyGenerated
 import Mathlib.Topology.IsLocalHomeomorph
 import Mathlib.Topology.Covering.Quotient
+import Mathlib.Tactic
 
 open Topology Manifold
 
@@ -83,6 +84,14 @@ lemma localInverseAt_apply_self {p : M} (hq : ⟦p⟧ ∈ (localInverseAt G p).s
   apply (aux G p).injOn ((localInverseAt G p).map_source hq) (aux_prop G p)
   simp only [localInverseAt, (aux G p).right_inv hq, aux_eq]
 
+/- confused? why do we need the hypothesis hq?
+lemma localInverseAt_apply_self {p : M} :
+    (localInverseAt G p) ⟦p⟧ = p := by
+  have hq : ⟦p⟧ ∈ (localInverseAt G p).source := by exact quotientMk_mem_localInverseAt_source G
+  apply (aux G p).injOn ((localInverseAt G p).map_source hq) (aux_prop G p)
+  simp only [localInverseAt, (aux G p).right_inv hq, aux_eq]
+-/
+
 --Given two points p and k in M s.t. k is in the domain of π_p and [k] is in the domain
 -- of (π_p)⁻¹, then (π_p)⁻¹([k]) = k.
 lemma localInverseAt_apply_other {p k : M} (hk : k ∈ (aux G p).source)
@@ -128,7 +137,7 @@ instance : ChartedSpace H (OrbitSpace M G) where
     simp only [myChartAt, OpenPartialHomeomorph.trans_toPartialEquiv, PartialEquiv.trans_source,
       OpenPartialHomeomorph.toFun_eq_coe, Set.mem_inter_iff, Set.mem_preimage]
     set p := q.out
-    rw [← q.out_eq, localInverseAt_apply_self (quotientMk_mem_localInverseAt_source G)]
+    rw [← q.out_eq, localInverseAt_apply_self]
     exact ⟨quotientMk_mem_localInverseAt_source G, mem_chart_source H p⟩
   chart_mem_atlas := by simp
 
@@ -281,40 +290,377 @@ lemma give_this_a_name (x y : OrbitSpace M G) :
   · -- φ' ∈ IsManifold.maximalAtlas I (↑n) M ?
     apply IsManifold.chart_mem_maximalAtlas
 
+
+
+
+
+example (x y : OrbitSpace M G) (p p' : M)
+    (hp : p = x.out) (hp' : p' = y.out)
+    (u u' : M)
+    (h : (aux G p) u = (aux G p') u')
+      -- basically h is saying that there is an intersection
+    :
+    (localInverseAt G (x.out)).symm ≫ₕ (localInverseAt G (y.out))
+      = fun m : M ↦ g0 h • m
+    := by
+  ext a
+  simp
+
+  set U := (aux G p).source
+  set U' := (aux G p').source
+  have h' := lemma3 G h U U'
+
+  -- π ( U ∩ g0⁻¹ (U') ) ∩ π ( U' ∩ g0 (U) ) =
+  -- = π ( g0 (U ∩ g0⁻¹ U') )
+
+
+
+
+
+
+
+
+
+  sorry
+
+
+
+/-- Note: we have this instance
+      instance :
+        Membership (OpenPartialHomeomorph H H) (StructureGroupoid H)
+        := ⟨fun (G : StructureGroupoid H) (e : OpenPartialHomeomorph H H) ↦ e ∈ G.members⟩
+
+      which means that an open partial homeomorphism can be a member of structured groupoid
+      if it is on its members
+-/
+
+example (p q : M) : (chartAt H p).symm ≫ₕ (chartAt H q) ∈ contDiffGroupoid (↑n) I := by
+  change (chartAt H p).symm ≫ₕ chartAt H q ∈ (contDiffGroupoid (↑n) I).members
+
+  have hp : chartAt H p ∈ atlas H M := by exact ChartedSpace.chart_mem_atlas p
+  have hq : chartAt H q ∈ atlas H M := by exact ChartedSpace.chart_mem_atlas q
+
+  have manifold : IsManifold I (↑n) M := by (expose_names; exact inst_6)
+  exact manifold.compatible hp hq
+
+
+example (q : OrbitSpace M G) :
+  (localInverseAt G (q.out)).symm = aux G q.out := by
+  exact rfl
+
+lemma if_source_of_first_empty_then_composition_empty {α β χ : Type} (f : PartialEquiv α β)
+  (g : PartialEquiv β χ) (h : f.source = ∅) : (f.trans g).source = ∅ := by
+
+  refine Set.eq_empty_of_forall_notMem ?_
+  intro z
+  by_contra c
+  obtain ⟨c1, c2⟩ := c
+  have aux'' : f.symm.target = f.source := by exact rfl
+  rw [aux'', h] at c1
+  exact c1
+
+
+
+lemma if_no_source_in_target_composition_empty {α β χ : Type} (f : PartialEquiv α β)
+  (g : PartialEquiv β χ) (h : g.source ∩ f.target = ∅) : (f.trans g).source = ∅ := by
+  refine Set.eq_empty_of_forall_notMem ?_
+  intro z
+  by_contra c
+  obtain ⟨c1, c2⟩ := c
+  rw [← Set.not_nonempty_iff_eq_empty, Set.inter_comm] at h
+  unfold Set.Nonempty at h
+  simp at h
+  specialize h (f z) (PartialEquiv.map_source f c1)
+  exact h c2
+
+
+lemma if_no_source_in_target_composition_empty_coerc
+    {α : Type u} {β : Type v} {χ : Type w}
+    [TopologicalSpace α] [TopologicalSpace β] [TopologicalSpace χ]
+    (f : OpenPartialHomeomorph α β)
+    (g : OpenPartialHomeomorph β χ) (h : g.source ∩ f.target = ∅) : (f.trans g).source = ∅ := by
+  refine Set.eq_empty_of_forall_notMem ?_
+  intro z
+  by_contra c
+  obtain ⟨c1, c2⟩ := c
+  rw [← Set.not_nonempty_iff_eq_empty, Set.inter_comm] at h
+  unfold Set.Nonempty at h
+  simp at h
+  specialize h (f z) (OpenPartialHomeomorph.map_source f c1)
+  exact h c2
+
+lemma if_source_of_first_empty_then_composition_empty_coerc
+    {α : Type u} {β : Type v} {χ : Type w}
+    [TopologicalSpace α] [TopologicalSpace β] [TopologicalSpace χ]
+    (f : OpenPartialHomeomorph α β)
+    (g : OpenPartialHomeomorph β χ) (h : f.source = ∅) : (f.trans g).source = ∅ := by
+  refine Set.eq_empty_of_forall_notMem ?_
+  intro z
+  by_contra c
+  obtain ⟨c1, c2⟩ := c
+  have aux'' : f.symm.target = f.source := by exact rfl
+  rw [aux'', h] at c1
+  exact c1
+
+lemma if_source_of_second_empty_then_composition_empty_coerc
+    {α : Type u} {β : Type v} {χ : Type w}
+    [TopologicalSpace α] [TopologicalSpace β] [TopologicalSpace χ]
+    (f : OpenPartialHomeomorph α β)
+    (g : OpenPartialHomeomorph β χ) (h : g.source = ∅) : (f.trans g).source = ∅ := by
+  apply if_no_source_in_target_composition_empty_coerc
+  rw [h]
+  simp
+
+
+
+
+#check PartialEquiv.EqOnSource
+#check PartialEquiv.trans_source
+
 -- And let's prove that it's a manifold.
 instance : IsManifold I n (OrbitSpace M G) where
-   compatible := by
+  compatible := by
     rintro _ _ ⟨x, rfl⟩ ⟨y, rfl⟩
+
+
     unfold myChartAt
 
-    set φ' := chartAt H (Quotient.out y)
-    set φ := chartAt H (Quotient.out x)
-    set π := localInverseAt G (Quotient.out x)
-    set π' := localInverseAt G (Quotient.out y)
+    --- renaming
+
+    set φy := chartAt H (Quotient.out y)
+    set φx := chartAt H (Quotient.out x)
+    set πinvx := localInverseAt G (Quotient.out x)
+    set πinvy := localInverseAt G (Quotient.out y)
 
     rw [OpenPartialHomeomorph.trans_symm_eq_symm_trans_symm]
+    nth_rw 1 [OpenPartialHomeomorph.trans_assoc]
+    nth_rw 2 [← OpenPartialHomeomorph.trans_assoc]
 
-    rw [OpenPartialHomeomorph.trans_assoc]
-    nth_rewrite 2 [← OpenPartialHomeomorph.trans_assoc]
+    unfold contDiffGroupoid
 
-    refine IsManifold.compatible_of_mem_maximalAtlas ?_ ?_
-    · apply IsManifold.chart_mem_maximalAtlas
-    · --#check IsManifold.mem_maximalAtlas_iff (H := H) (M := M) (e : φ')
+    apply StructureGroupoid.locality
+
+    intro h hh
+    simp at hh
+    obtain ⟨hh1, ⟨hh2, hh3⟩, hh4⟩ := hh
+
+    set Up := πinvx.target ∩ φx.source
+    set Uq := πinvy.target ∩ φy.source
+
+    have hUp : φx.symm h ∈ Up := by
+      refine ⟨hh2, ?_⟩
+      exact OpenPartialHomeomorph.map_target φx hh1
+
+    have hUq : πinvy (πinvx.symm (φx.symm h)) ∈ Uq := by
+      refine ⟨?_, hh4⟩
+      exact OpenPartialHomeomorph.map_source πinvy hh3
+
+    have heq : πinvx.symm (φx.symm h) = πinvy.symm (πinvy (πinvx.symm (φx.symm h))) := by
+      rw [OpenPartialHomeomorph.left_inv]
+      exact hh3
+
+    have lemma2 := lemma2 heq Up Uq
+    have lemma3 := lemma3 G heq Up Uq
+
+    set ρ : M → M := fun x_1 ↦ g0 heq • x_1
+    set ρinv : M → M := fun x_1 ↦ (g0 heq)⁻¹ • x_1
+    set Up' := Up ∩ ρinv '' Uq
+    set Uq' := Uq ∩ ρ '' Up
+
+    use ((πinvx ≫ₕ φx) '' ((π (G := G) '' Up') ∩ (π (G := G) '' Uq')))
+
+    constructor
+    ·
+      -- is open s
+      sorry
+
+    constructor
+
+    · -- h in s
+      sorry
+
+    constructor
+
+    ·
+      sorry
+    ·
+      sorry
+
+
+
+
+
+/-
+
+
+
+
+
+    have aux : ∃ g : G, ρ g = (πinvx.symm ≫ₕ πinvy) := by
+      sorry
+
+    obtain ⟨g, hρ⟩ := aux
+
+
+
+
+
+    --rw [OpenPartialHomeomorph.trans_symm_eq_symm_trans_symm]
+
+    --have def_πinv : (localInverseAt G (x.out)).symm = aux G x.out := by exact rfl
+
+    --rw [def_πinv]
+    --set πx := aux G x.out
+
+    by_cases h : (∃ z, z ∈ (πinvx.symm.target ∩ πinvy.source))
+
+    · obtain ⟨u, hu⟩ := h
+      obtain ⟨hu1, hu2⟩ := hu
+      simp at hu1
+
+      have hu1' := hu1
+      apply PartialEquiv.map_target' at hu1'
+      have hu2' := hu2
+      apply PartialEquiv.map_target' at hu2'
+      set πy := aux G (Quotient.out y)
+
+      /-
+      Because u is in the source of both πx⁻¹ and πy⁻¹
+      we can take
+      - u1 = πx⁻¹ (u)
+      - u2 = πy⁻¹ (u)
+      and this way πx(u1) = πy(u2) = u
+      -/
+
+      have huxy : πinvx.symm (πinvx.symm.invFun u) = πy (πy.invFun u) := by
+        simp
+        rw [OpenPartialHomeomorph.right_inv, OpenPartialHomeomorph.left_inv]
+        · exact hu1
+        exact hu2
+
+      set Up := πinvx.target ∩ φx.source
+      set Uq := πinvy.target ∩ φy.source
+
+      --have lemma1 := lemma1 huxy
+
+      have lemma2 := lemma2 huxy Up Uq
+      have lemma3 := lemma3 G huxy Up Uq
+
+      set ρ : M → M := fun x_1 ↦ g0 huxy • x_1
+      set ρinv : M → M := fun x_1 ↦ (g0 huxy)⁻¹ • x_1
+      set Up' := Up ∩ ρinv '' Uq
+      set Uq' := Uq ∩ ρ '' Up
+
+      -- up until here i think im following the paper
+
+      have bigsource : ((πinvx ≫ₕ φx).symm ≫ₕ πinvy ≫ₕ φy).source ⊆
+        {u : H | (πinvx ≫ₕ φx).symm u ∈ (πinvx.source ∩ πinvy.source)} := by
+        intro t ht
+        simp
+        obtain ⟨ht1, ht2⟩ := ht
+        simp at ht1
+        simp at ht2
+        have ht1 := ht1.right
+        have ht2 := ht2.left
+        constructor
+        · exact OpenPartialHomeomorph.map_target πinvx ht1
+        exact ht2
+
+      have s : ((πinvx ≫ₕ φx).symm ≫ₕ πinvy ≫ₕ φy).source
+          ⊆ (πinvx ≫ₕ φx) '' (π '' Up' ∩ π '' Uq') := by
+        intro h hh
+        simp at hh
+        obtain ⟨⟨hh1, hh2⟩, hh3, hh4⟩ := hh
+        simp
+        use φx.symm h
+        constructor
+        constructor
+        · sorry
+        · sorry
+        · sorry
+
+
+
+
+      set Vx := πinvx.source
+      set Vy := πinvy.source
+
+      set Ux := (πinvx '' Vx)
+      set Uy := (πinvy '' Vy)
+
+
+
+      have lemma1 := lemma1 huxy
+
+
+      set G0 := (fun x_1 : M ↦ g0 huxy • x_1)
+      set G0inv := (fun x_1 : M ↦ (g0 huxy)⁻¹ • x_1)
+
+      set Ux' := Ux ∩ G0inv '' Uy
+      set Uy' := Uy ∩ G0 '' Ux
+
+
+
+      set e := (πinvx ≫ₕ φx)
+
+      set v := e u
+
+      have step0 : v ∈ e '' (Vx ∩ Vy) := by
+        use u
+        exact ⟨⟨hu1, hu2⟩, rfl⟩
+
+      have bigsource' : ((πinvx ≫ₕ φx).symm ≫ₕ πinvy ≫ₕ φy).source ⊆
+        e '' (π '' Ux' ∩ π '' Uy') := by
+        intro t ht
+        apply bigsource at ht
+        simp at ht
+        use e.symm t
+        constructor
+        constructor
+        · simp
+          use πinvx (e.symm t)
+          constructor
+          · constructor
+            · use e.symm t
+              exact ⟨ht.left, rfl⟩
+            · simp
+
+              sorry
+          · sorry
+        sorry
+        sorry -- should be trivial
+
+
+      have step2 : v ∈ e '' (π '' (G0 '' Ux)) := by
+        --rw [← lemma2]
+        sorry
+
+      --set Ux := πx.source
+      --set Uy := πy.source
+
+      rw [lemma2] at lemma3
+
 
       sorry
 
 
-    --- then apply associativity of >>h -> so we will get three components:
-    -- φ⁻¹ ∘ (πₚ⁻¹ ∘ πₚ') ∘ φ' (or something like this)
+    · --- esto me puede servir luego
+      rw [OpenPartialHomeomorph.trans_symm_eq_symm_trans_symm]
 
-    -- then prove : 1. φ's are differentiable (already true since M is already smooth manifold)
-    -- 2. (πₚ⁻¹ ∘ πₚ') is actually the action of 1 element of the group
-    -- so then is is differentiable as well ()
+      have def_πinv : (localInverseAt G (x.out)).symm = aux G x.out := by exact rfl
 
+      rw [def_πinv]
+      set πx := aux G x.out
 
-
-
-
+      rw [OpenPartialHomeomorph.trans_assoc]
+      nth_rewrite 2 [← OpenPartialHomeomorph.trans_assoc]
+      apply ContDiffGroupoid.mem_of_source_eq_empty
+      apply if_source_of_second_empty_then_composition_empty_coerc φx.symm ((πx ≫ₕ πinvy) ≫ₕ φy)
+      apply if_source_of_first_empty_then_composition_empty_coerc (πx ≫ₕ πinvy) φy
+      apply if_no_source_in_target_composition_empty_coerc πx πinvy
+      rw [Set.inter_comm]
+      exact Set.not_nonempty_iff_eq_empty.mp h
+-/
 
 
 -- Once we have done this, let's prove that the projection map is smooth.
